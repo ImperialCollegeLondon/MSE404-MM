1ee99d2a82dd220c53b3f69ed47edbb607c42fe9
jae23 <edwardsjordan925@gmail.com>
2024-09-24 11:34:13 +0100

    Update lab03, add convergence_script.

---
+++ docs/labs/convergence_scripts/ecutwfc_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run scf calculations.
+for i in {001..010};
+do
+mpiexec pw.x < scf.mol.$i.in > scf.mol.$i.out
+done
+
+# Loop through files
+for i in {001..010}; do
+    # Extract 'ecutwfc' value from input molecule files
+    ecutwfc_value=$(grep 'ecutwfc' scf.mol.$i.in | awk '{print $3}' | tr -d ', =')
+
+    # Extract 'Total Energy' value from input molecule files
+    final_energy_value=$(grep '!' scf.mol.$i.out | awk '{print $5}')
+
+    # Multiply 'final_energy_value' by 13.6
+    final_energy_value=$(echo "$final_energy_value * 13.6" | bc)
+
+    echo -e "$ecutwfc_value $final_energy_value" >> data.txt
+done
---
+++ docs/labs/lab03/03_argon/01_lda/file_builder.py
@@ -0,0 +1,61 @@
+##############################################################################################################################################
+##############################################################################################################################################
+##############################################################################################################################################
+#                           This is a python file that will generate multiple input files for a convergence test.                            #
+#                                                                                                                                            #
+#                   How to use: Copy and paste the text from your input file to common_content_template as shown below.                      #
+#                               Make sure you are putting a space both sides of = sign when pasting.                                         #
+#                                                                                                                                            #
+#                                                   How to run: python3 analysis.py                                                          #
+##############################################################################################################################################
+##############################################################################################################################################
+##############################################################################################################################################
+
+
+# Directory where you want to create the files
+output_directory = "./"
+
+# Number of files to create
+num_files = 20
+
+# Common content template with a placeholder for the number
+common_content_template = """ &CONTROL
+    pseudo_dir = '.'
+    disk_io = 'none'
+ /
+
+ &SYSTEM
+    ibrav = 1
+    A = 12.0
+    nat = 2
+    ntyp = 1
+    ecutwfc = 30.0
+ /
+
+ &ELECTRONS
+    ! This is the default
+    conv_thr = 1.0E-6
+ /
+
+ATOMIC_SPECIES
+ Ar  39.948  Ar.pz-n-rrkjus_psl.1.0.0.UPF
+
+ATOMIC_POSITIONS angstrom
+ Ar  0.0       0.0       0.0
+ Ar  0.0       0.0       {}
+
+K_POINTS gamma
+"""
+
+# Loop to create the files
+for i in range(1, num_files + 1):
+    # Define the content for each file with the number replaced
+    calc = round(3+0.1*i, 3)
+    content = common_content_template.format(calc)
+
+    # Generate the file name
+    file_name = f"{output_directory}/scf.mol.{str(i).zfill(3)}.in"
+
+    # Open and write to the file directly
+    with open(file_name, 'w') as file:
+        file.write(content)
